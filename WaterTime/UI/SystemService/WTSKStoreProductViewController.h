d693783532ffd86f527d8cdf9805a92cb9236be9
WaterLiu <WaterLiu@WaterLius-MacBook-Pro.local>
2015-06-10 19:29:18 +0800

    添加AppStore

---
+++ WaterTime/UI/SystemService/WTSKStoreProductViewController.h
@@ -0,0 +1,13 @@
+//
+//  WTSKStoreProductViewController.h
+//  WaterTime
+//
+//  Created by WaterLiu on 15/6/10.
+//  Copyright (c) 2015年 WaterLiu. All rights reserved.
+//
+
+#import "WTCommonBaseViewController.h"
+
+@interface WTSKStoreProductViewController : WTCommonBaseViewController
+
+@end
---
+++ WaterTime/UI/SystemService/WTSKStoreProductViewController.m
@@ -0,0 +1,37 @@
+//
+//  WTSKStoreProductViewController.m
+//  WaterTime
+//
+//  Created by WaterLiu on 15/6/10.
+//  Copyright (c) 2015年 WaterLiu. All rights reserved.
+//
+
+#import "WTSKStoreProductViewController.h"
+
+@interface WTSKStoreProductViewController ()
+
+@end
+
+@implementation WTSKStoreProductViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
