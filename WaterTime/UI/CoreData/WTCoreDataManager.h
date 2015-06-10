e4e3179d4e781d2920a8d971cfb299700c1c4d64
WaterLiu <WaterLiu@WaterLius-MacBook-Pro.local>
2015-06-10 17:56:16 +0800

    添加逻辑

---
+++ WaterTime/UI/CoreData/WTCoreDataManager.h
@@ -0,0 +1,13 @@
+//
+//  WTCoreDataManager.h
+//  WaterTime
+//
+//  Created by WaterLiu on 15/2/6.
+//  Copyright (c) 2015年 WaterLiu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WTCoreDataManager : NSObject
+
+@end
---
+++ WaterTime/UI/CoreData/WTCoreDataManager.m
@@ -0,0 +1,13 @@
+//
+//  WTCoreDataManager.m
+//  WaterTime
+//
+//  Created by WaterLiu on 15/2/6.
+//  Copyright (c) 2015年 WaterLiu. All rights reserved.
+//
+
+#import "WTCoreDataManager.h"
+
+@implementation WTCoreDataManager
+
+@end
