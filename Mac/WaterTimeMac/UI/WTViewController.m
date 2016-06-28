7e2f414630336977c61a359b6963785f93759747
water1984609 <water609@qq.com>
2016-06-28 15:34:53 +0800

    no message

--- Mac/WaterTimeMac/UI/WTViewController.m
+++ Mac/WaterTimeMac/UI/WTViewController.m
@@ -28,7 +28,8 @@
 
 - (void)loadView
 {
-    self.view = [[WTView alloc] initWithFrame:NSMakeRect(0.0f, 0.0f, 200.0f, 400.0f)];
+    NSWindow* window = [[NSApplication sharedApplication].windows objectAtIndex:0];
+    self.view = [[WTView alloc] initWithFrame:NSMakeRect(0.0f, 0.0f, CGRectGetWidth(window.frame), CGRectGetHeight(window.frame))];
     ((WTView*)self.view).backgroundColor = [NSColor redColor];
 }
 
@@ -38,29 +39,19 @@
     // Do view setup here.
     
     NSScrollView * tableContainer = [[NSScrollView alloc] initWithFrame:self.view.bounds];
+    NSTableColumn* cloumn =  [[NSTableColumn alloc] initWithIdentifier:@"Cloumn1"];
     
     _tableView = [[NSTableView alloc] initWithFrame:self.view.bounds];
     _tableView.delegate = self;
     _tableView.dataSource = self;
     _tableView.backgroundColor = [NSColor yellowColor];
-
-    
-    NSTableColumn* cloumn =  [[NSTableColumn alloc] initWithIdentifier:@"Cloumn1"];
-
+    _tableView.headerView = nil;
+    _tableView.floatsGroupRows = YES;
     
     [_tableView addTableColumn:cloumn];
-    
-    
     tableContainer.documentView = _tableView;
     [tableContainer setHasVerticalScroller:YES];
     [self.view addSubview:tableContainer];
-    
-    
-    
-    
-    
-    
-    
 }
 
 
@@ -85,28 +76,55 @@
 
 
 #pragma mark - NSTableViewDelegate
+
 - (nullable NSView *)tableView:(NSTableView *)tableView viewForTableColumn:(nullable NSTableColumn *)tableColumn row:(NSInteger)row
 {
-    
-    id view = [tableView makeViewWithIdentifier:@"WTTableCellView" owner:nil];
-    
-    if (view != nil)
+    if (row == 0 || row == 20)
     {
-        NSLog(@"111 = %ld", (long)row);
+        NSTextField* textField = [[NSTextField alloc] init];
+        [textField setStringValue:@"water"];
+        textField.backgroundColor = [NSColor redColor];
+        return textField;
+    }
+    
+    NSTableCellView* cellView = [tableView makeViewWithIdentifier:@"WTTableCellView" owner:nil];
+    if (cellView == nil)
+    {
+        cellView = [[WTTableCellView alloc] init];
+        cellView.identifier = @"WTTableCellView";
+        [cellView setWantsLayer:YES];
+        cellView.layer.backgroundColor = [NSColor blueColor].CGColor;
+    }
+    
+    
+    
+//    [cellView.textField setStringValue:@"water"];
+    return cellView;
+}
+
+
+- (BOOL)tableView:(NSTableView *)tableView isGroupRow:(NSInteger)row
+{
+    if (row == 0 || row == 20)
+    {
+        return YES;
     }
     else
     {
-        NSLog(@"222 = %ld",(long)row);
+        return NO;
     }
+}
 
-
-    WTTableCellView* cell = [[WTTableCellView alloc] init];
-    
-    cell.identifier = @"WTTableCellView";
-
-//    [_cellArray addObject:cell];
-    
-    return cell;
+- (CGFloat)tableView:(NSTableView *)tableView heightOfRow:(NSInteger)row
+{
+    if (row == 0 || row == 20)
+    {
+        return 30.0f;
+    }
+    else
+    {
+        return 20.0f;
+    }
 }
 
 @end
