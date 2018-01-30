ae9e53f3088e41129ff523a845e2d5e00e10e2ba
WaterLiu <water1984609@hotmail.com>
2018-01-30 22:53:07 +0800

    提交

---
+++ Mac/WaterTimeMac/UI/Window/WTWindowController.h
@@ -0,0 +1,13 @@
+//
+//  WTWindowController.h
+//  WaterTimeMac
+//
+//  Created by WaterLiu on 2018/1/30.
+//  Copyright © 2018年 WaterLiu. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface WTWindowController : NSWindowController
+
+@end
---
+++ Mac/WaterTimeMac/UI/Window/WTWindowController.m
@@ -0,0 +1,23 @@
+//
+//  WTWindowController.m
+//  WaterTimeMac
+//
+//  Created by WaterLiu on 2018/1/30.
+//  Copyright © 2018年 WaterLiu. All rights reserved.
+//
+
+#import "WTWindowController.h"
+
+@interface WTWindowController ()
+
+@end
+
+@implementation WTWindowController
+
+- (void)windowDidLoad {
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
